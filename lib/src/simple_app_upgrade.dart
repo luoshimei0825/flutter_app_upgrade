e98bcbcf8794e1210e762487b4ba6ed6d275855b
luoshimei0825 <0x005168@gmail.com>
2021-07-13 16:30:53 +0800

    support Null savety

--- lib/src/simple_app_upgrade.dart
+++ lib/src/simple_app_upgrade.dart
@@ -11,37 +11,38 @@ import 'liquid_progress_indicator.dart';
 /// des:app升级提示控件
 ///
 class SimpleAppUpgradeWidget extends StatefulWidget {
-  const SimpleAppUpgradeWidget(
-      {@required this.title,
-      this.titleStyle,
-      @required this.contents,
-      this.contentStyle,
-      this.cancelText,
-      this.cancelTextStyle,
-      this.okText,
-      this.okTextStyle,
-      this.okBackgroundColors,
-      this.progressBar,
-      this.progressBarColor,
-      this.borderRadius = 10,
-      this.downloadUrl,
-      this.force = false,
-      this.iosAppId,
-      this.appMarketInfo,
-      this.onCancel,
-      this.onOk,
-      this.downloadProgress,
-      this.downloadStatusChange});
+  const SimpleAppUpgradeWidget({
+    required this.title,
+    this.titleStyle,
+    required this.contents,
+    this.contentStyle,
+    this.cancelText,
+    this.cancelTextStyle,
+    this.okText,
+    this.okTextStyle,
+    this.okBackgroundColors,
+    this.progressBar,
+    this.progressBarColor,
+    this.borderRadius = 10,
+    this.downloadUrl,
+    this.force = false,
+    this.iosAppId,
+    this.appMarketInfo,
+    this.onCancel,
+    this.onOk,
+    this.downloadProgress,
+    this.downloadStatusChange,
+  });
 
   ///
   /// 升级标题
   ///
-  final String title;
+  final String? title;
 
   ///
   /// 标题样式
   ///
-  final TextStyle titleStyle;
+  final TextStyle? titleStyle;
 
   ///
   /// 升级提示内容
@@ -51,47 +52,47 @@ class SimpleAppUpgradeWidget extends StatefulWidget {
   ///
   /// 提示内容样式
   ///
-  final TextStyle contentStyle;
+  final TextStyle? contentStyle;
 
   ///
   /// 下载进度条
   ///
-  final Widget progressBar;
+  final Widget? progressBar;
 
   ///
   /// 进度条颜色
   ///
-  final Color progressBarColor;
+  final Color? progressBarColor;
 
   ///
   /// 确认控件
   ///
-  final String okText;
+  final String? okText;
 
   ///
   /// 确认控件样式
   ///
-  final TextStyle okTextStyle;
+  final TextStyle? okTextStyle;
 
   ///
   /// 确认控件背景颜色,2种颜色左到右线性渐变
   ///
-  final List<Color> okBackgroundColors;
+  final List<Color>? okBackgroundColors;
 
   ///
   /// 取消控件
   ///
-  final String cancelText;
+  final String? cancelText;
 
   ///
   /// 取消控件样式
   ///
-  final TextStyle cancelTextStyle;
+  final TextStyle? cancelTextStyle;
 
   ///
   /// app安装包下载url,没有下载跳转到应用宝等渠道更新
   ///
-  final String downloadUrl;
+  final String? downloadUrl;
 
   ///
   /// 圆角半径
@@ -106,18 +107,18 @@ class SimpleAppUpgradeWidget extends StatefulWidget {
   ///
   /// ios app id,用于跳转app store
   ///
-  final String iosAppId;
+  final String? iosAppId;
 
   ///
   /// 指定跳转的应用市场，
   /// 如果不指定将会弹出提示框，让用户选择哪一个应用市场。
   ///
-  final AppMarketInfo appMarketInfo;
+  final AppMarketInfo? appMarketInfo;
 
-  final VoidCallback onCancel;
-  final VoidCallback onOk;
-  final DownloadProgressCallback downloadProgress;
-  final DownloadStatusChangeCallback downloadStatusChange;
+  final VoidCallback? onCancel;
+  final VoidCallback? onOk;
+  final DownloadProgressCallback? downloadProgress;
+  final DownloadStatusChangeCallback? downloadStatusChange;
 
   @override
   State<StatefulWidget> createState() => _SimpleAppUpgradeWidget();
@@ -173,9 +174,7 @@ class _SimpleAppUpgradeWidget extends State<SimpleAppUpgradeWidget> {
   ///
   _buildTitle() {
     return Padding(
-        padding: EdgeInsets.only(top: 20, bottom: 30),
-        child: Text(widget.title ?? '',
-            style: widget.titleStyle ?? TextStyle(fontSize: 22)));
+        padding: EdgeInsets.only(top: 20, bottom: 30), child: Text(widget.title ?? '', style: widget.titleStyle ?? TextStyle(fontSize: 22)));
   }
 
   ///
@@ -226,17 +225,13 @@ class _SimpleAppUpgradeWidget extends State<SimpleAppUpgradeWidget> {
   ///
   _buildCancelActionButton() {
     return Ink(
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(widget.borderRadius))),
+      decoration: BoxDecoration(borderRadius: BorderRadius.only(bottomLeft: Radius.circular(widget.borderRadius))),
       child: InkWell(
-          borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(widget.borderRadius)),
+          borderRadius: BorderRadius.only(bottomLeft: Radius.circular(widget.borderRadius)),
           child: Container(
             height: 45,
             alignment: Alignment.center,
-            child: Text(widget.cancelText ?? '以后再说',
-                style: widget.cancelTextStyle ?? TextStyle()),
+            child: Text(widget.cancelText ?? '以后再说', style: widget.cancelTextStyle ?? TextStyle()),
           ),
           onTap: () {
             widget.onCancel?.call();
@@ -249,35 +244,24 @@ class _SimpleAppUpgradeWidget extends State<SimpleAppUpgradeWidget> {
   /// 确定按钮
   ///
   _buildOkActionButton() {
-    var borderRadius =
-        BorderRadius.only(bottomRight: Radius.circular(widget.borderRadius));
+    var borderRadius = BorderRadius.only(bottomRight: Radius.circular(widget.borderRadius));
     if (widget.force) {
-      borderRadius = BorderRadius.only(
-          bottomRight: Radius.circular(widget.borderRadius),
-          bottomLeft: Radius.circular(widget.borderRadius));
+      borderRadius = BorderRadius.only(bottomRight: Radius.circular(widget.borderRadius), bottomLeft: Radius.circular(widget.borderRadius));
     }
     var _okBackgroundColors = widget.okBackgroundColors;
-    if (widget.okBackgroundColors == null ||
-        widget.okBackgroundColors.length != 2) {
-      _okBackgroundColors = [
-        Theme.of(context).primaryColor,
-        Theme.of(context).primaryColor
-      ];
+    if (widget.okBackgroundColors == null || widget.okBackgroundColors!.length != 2) {
+      _okBackgroundColors = [Theme.of(context).primaryColor, Theme.of(context).primaryColor];
     }
     return Ink(
       decoration: BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [_okBackgroundColors[0], _okBackgroundColors[1]]),
+          gradient: LinearGradient(begin: Alignment.topLeft, end: Alignment.bottomRight, colors: [_okBackgroundColors![0], _okBackgroundColors[1]]),
           borderRadius: borderRadius),
       child: InkWell(
         borderRadius: borderRadius,
         child: Container(
           height: 45,
           alignment: Alignment.center,
-          child: Text(widget.okText ?? '立即体验',
-              style: widget.okTextStyle ?? TextStyle(color: Colors.white)),
+          child: Text(widget.okText ?? '立即体验', style: widget.okTextStyle ?? TextStyle(color: Colors.white)),
         ),
         onTap: () {
           _clickOk();
@@ -294,8 +278,7 @@ class _SimpleAppUpgradeWidget extends State<SimpleAppUpgradeWidget> {
         LiquidLinearProgressIndicator(
           value: _downloadProgress,
           direction: Axis.vertical,
-          valueColor: AlwaysStoppedAnimation(widget.progressBarColor ??
-              Theme.of(context).primaryColor.withOpacity(0.4)),
+          valueColor: AlwaysStoppedAnimation(widget.progressBarColor ?? Theme.of(context).primaryColor.withOpacity(0.4)),
           borderRadius: widget.borderRadius,
         );
   }
@@ -305,12 +288,12 @@ class _SimpleAppUpgradeWidget extends State<SimpleAppUpgradeWidget> {
   ///
   _clickOk() async {
     widget.onOk?.call();
-    if (Platform.isIOS) {
-      //ios 需要跳转到app store更新，原生实现
-      FlutterUpgrade.toAppStore(widget.iosAppId);
+    if (Platform.isIOS && widget.iosAppId != null) {
+      // ios 需要跳转到app store更新，原生实现
+      FlutterUpgrade.toAppStore(widget.iosAppId!);
       return;
     }
-    if (widget.downloadUrl == null || widget.downloadUrl.isEmpty) {
+    if (widget.downloadUrl == null || widget.downloadUrl!.isEmpty) {
       //没有下载地址，跳转到第三方渠道更新，原生实现
       FlutterUpgrade.toMarket(appMarketInfo: widget.appMarketInfo);
       return;
@@ -322,10 +305,8 @@ class _SimpleAppUpgradeWidget extends State<SimpleAppUpgradeWidget> {
   ///
   /// 下载apk包
   ///
-  _downloadApk(String url, String path) async {
-    if (_downloadStatus == DownloadStatus.start ||
-        _downloadStatus == DownloadStatus.downloading ||
-        _downloadStatus == DownloadStatus.done) {
+  _downloadApk(String? url, String path) async {
+    if (_downloadStatus == DownloadStatus.start || _downloadStatus == DownloadStatus.downloading || _downloadStatus == DownloadStatus.done) {
       print('当前下载状态：$_downloadStatus,不能重复下载。');
       return;
     }
@@ -333,7 +314,7 @@ class _SimpleAppUpgradeWidget extends State<SimpleAppUpgradeWidget> {
     _updateDownloadStatus(DownloadStatus.start);
     try {
       var dio = Dio();
-      await dio.download(url, path, onReceiveProgress: (int count, int total) {
+      await dio.download(url!, path, onReceiveProgress: (int count, int total) {
         if (total == -1) {
           _downloadProgress = 0.01;
         } else {
@@ -351,7 +332,7 @@ class _SimpleAppUpgradeWidget extends State<SimpleAppUpgradeWidget> {
     } catch (e) {
       print('$e');
       _downloadProgress = 0;
-      _updateDownloadStatus(DownloadStatus.error,error: e);
+      _updateDownloadStatus(DownloadStatus.error, error: e);
     }
   }
 
